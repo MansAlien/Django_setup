3bd0359447ac8c0317c9518ae9e4e470cd6bca73
MansAlien <mans012033945421@gmail.com>
2024-01-09 10:42:46 +0200

    update the django_setup file

--- django_setup.sh
+++ django_setup.sh
@@ -1,6 +1,10 @@
+sudo rm -r .git
 python -m venv .venv
 source .venv/bin/activate
 pip install --upgrade pip
-pip install Django
-pip install psycopg2-binary
+pip install -r requirements.txt
 django-admin startproject project .
+git init
+git add .
+git commit -m "Initial commit"
+git branch -M main
