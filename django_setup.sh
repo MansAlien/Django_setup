a7aa0c8e0d6c90cb752f955ea5bf7efff0005ada
MansAlien <mans012033945421@gmail.com>
2024-01-09 11:07:16 +0200

    update

--- django_setup.sh
+++ django_setup.sh
@@ -4,9 +4,10 @@ source .venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
 django-admin startproject project .
+echo .venv > .gitignore
+echo django_setup.sh >> .gitignore
 git init
 git add .
 git commit -m "Initial commit"
 git branch -M main
-echo .venv > .gitignore
-echo django_setup.sh >> .gitignore
+
