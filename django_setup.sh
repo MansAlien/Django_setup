dc43fde66ba625ba968a31d4e101cfa5f7c62c44
MansAlien <mans012033945421@gmail.com>
2024-01-19 19:01:32 +0200

    adding the settings and urls and wsgi to project

--- django_setup.sh
+++ django_setup.sh
@@ -6,6 +6,12 @@ pip install -r requirements.txt
 django-admin startproject project .
 echo .venv > .gitignore
 echo django_setup.sh >> .gitignore
+rm -r project/settings.py
+rm -r project/wsgi.py
+rm -r project/urls.py
+mv settings.py proejct/
+mv wsgi.py proejct/
+mv urls.py proejct/
 git init
 git add .
 git commit -m "Initial commit"
